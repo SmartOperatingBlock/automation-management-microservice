43f9ba4670d1d7b21f8ba8faaddc7b1a02b03fd4
andrea.giulianelli <andrygiulio@gmail.com>
2023-03-17 11:40:59 +0100

    build: convert build.gradle from groovy to kotlin and add toml catalog

---
+++ build.gradle.kts
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2023. Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+plugins {
+    application
+    java
+    eclipse
+}
+
+group = "io.github.smartoperatingblock"
+
+repositories {
+    mavenCentral()
+    maven {
+        url = uri("https://raw.githubusercontent.com/jacamo-lang/mvn-repo/master")
+    }
+    maven {
+        url = uri("https://repo.gradle.org/gradle/libs-releases")
+    }
+}
+
+dependencies {
+    implementation(libs.jacamo)
+    testImplementation(libs.junit.api)
+    testRuntimeOnly(libs.junit.engine)
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(15))
+    }
+}
+
+sourceSets.getByName("main") {
+    java.srcDir("src/env")
+    java.srcDir("src/agt")
+    java.srcDir("src/org")
+    java.srcDir("src/int")
+    java.srcDir("src/java")
+    resources.srcDir("src/resources")
+}
+
+tasks.named<JavaExec>("run") {
+    dependsOn("classes")
+    group = "JaCaMo"
+    description = "runs the JaCaMo application"
+    doFirst {
+        mkdir("log")
+    }
+    mainClass.set("jacamo.infra.JaCaMoLauncher")
+    args("automation_management_microservice.jcm")
+    classpath = project.sourceSets.main.get().runtimeClasspath
+}
+
+tasks.named("clean") {
+    delete("bin")
+    buildDir.deleteRecursively()
+    delete("log")
+}
