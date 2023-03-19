78907f2f8901784baa65dab5d7c19721ead92ef4
Andrea Giulianelli <andrygiulio@gmail.com>
2023-03-19 11:16:33 +0100

    build: enable additional information for tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,6 +32,13 @@ dependencies {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+    testLogging {
+        showStandardStreams = true
+        showCauses = true
+        showStackTraces = true
+        events(*org.gradle.api.tasks.testing.logging.TestLogEvent.values())
+        exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+    }
 }
 
 java {
