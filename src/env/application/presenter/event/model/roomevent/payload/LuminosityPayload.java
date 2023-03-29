afc5f32a18202d5c3f6cf8c2f0c378038c1531d1
andrea.giulianelli <andrygiulio@gmail.com>
2023-03-29 11:32:54 +0200

    chore: create room event luminosity payload

---
+++ src/env/application/presenter/event/model/roomevent/payload/LuminosityPayload.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2023. Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package application.presenter.event.model.roomevent.payload;
+
+/**
+ * Luminosity payload for {@link application.presenter.event.model.roomevent.RoomEvent}.
+ */
+public class LuminosityPayload implements RoomEventPayload {
+    private final double luminosityValue;
+    private final String luminosityUnit;
+
+    /**
+     * Default constructor.
+     * @param luminosityValue the luminosity value.
+     * @param luminosityUnit the luminosity unit.
+     */
+    public LuminosityPayload(final double luminosityValue, final String luminosityUnit) {
+        this.luminosityValue = luminosityValue;
+        this.luminosityUnit = luminosityUnit;
+    }
+
+    /**
+     * Get the luminosity value.
+     * @return the luminosity.
+     */
+    public double getLuminosityValue() {
+        return this.luminosityValue;
+    }
+
+    /**
+     * Get the luminosity unit.
+     * @return the luminosity unit.
+     */
+    public String getLuminosityUnit() {
+        return this.luminosityUnit;
+    }
+}
