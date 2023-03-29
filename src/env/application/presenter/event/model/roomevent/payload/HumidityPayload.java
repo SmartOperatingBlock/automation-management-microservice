55a015cb21504105c0c409373dc86dd2a9415c8a
andrea.giulianelli <andrygiulio@gmail.com>
2023-03-29 11:31:59 +0200

    chore: create room event humidity payload

---
+++ src/env/application/presenter/event/model/roomevent/payload/HumidityPayload.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2023. Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package application.presenter.event.model.roomevent.payload;
+
+/**
+ * Humidity payload for {@link application.presenter.event.model.roomevent.RoomEvent}.
+ */
+public class HumidityPayload implements RoomEventPayload {
+    private final int humidityPercentage;
+
+    /**
+     * Default constructor.
+     * @param humidityPercentage the humidity percentage.
+     */
+    public HumidityPayload(final int humidityPercentage) {
+        this.humidityPercentage = humidityPercentage;
+    }
+
+    /**
+     * Get the Humidity percentage.
+     * @return the humidity percentage.
+     */
+    public int getHumidityPercentage() {
+        return this.humidityPercentage;
+    }
+}
