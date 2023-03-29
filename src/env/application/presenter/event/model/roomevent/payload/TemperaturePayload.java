1991feb1becf234471cb6d4e94c89c6f7d12df1f
andrea.giulianelli <andrygiulio@gmail.com>
2023-03-29 11:55:36 +0200

    chore: create room event temperature payload

---
+++ src/env/application/presenter/event/model/roomevent/payload/TemperaturePayload.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2023. Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package application.presenter.event.model.roomevent.payload;
+
+/**
+ * Temperature payload for {@link application.presenter.event.model.roomevent.RoomEvent}.
+ */
+public class TemperaturePayload implements RoomEventPayload {
+    private final double temperatureValue;
+    private final String temperatureUnit;
+
+    /**
+     * Default constructor.
+     * @param temperatureValue the temperature value.
+     * @param temperatureUnit the temperature unit.
+     */
+    public TemperaturePayload(final double temperatureValue, final String temperatureUnit) {
+        this.temperatureValue = temperatureValue;
+        this.temperatureUnit = temperatureUnit;
+    }
+
+    /**
+     * Get the temperature value.
+     * @return the temperature value.
+     */
+    public double getTemperatureValue() {
+        return this.temperatureValue;
+    }
+
+    /**
+     * Get the temperature unit.
+     * @return the temperature unit.
+     */
+    public String getTemperatureUnit() {
+        return this.temperatureUnit;
+    }
+}
