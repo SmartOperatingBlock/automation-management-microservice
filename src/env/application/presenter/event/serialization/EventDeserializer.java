1e34931f9c3908dc516071eeb07c67cb8b1e7cc0
andrea.giulianelli <andrygiulio@gmail.com>
2023-03-30 10:39:05 +0200

    chore: create event deserializer interface

---
+++ src/env/application/presenter/event/serialization/EventDeserializer.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2023. Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package application.presenter.event.serialization;
+
+import application.presenter.event.model.Event;
+
+/**
+ * Interface that models an event deserializer.
+ */
+public interface EventDeserializer {
+    /**
+     * Deserialize an event from string.
+     * @param eventKey the key of the event.
+     * @param event the event in string format.
+     * @return the deserialized event.
+     */
+    Event<?> fromString(String eventKey, String event);
+}
