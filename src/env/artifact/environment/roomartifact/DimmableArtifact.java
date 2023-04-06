2f71ef9938fcb22a605b17d46d22aa90ab29bfb2
Andrea Giulianelli <andrygiulio@gmail.com>
2023-04-06 16:42:30 +0200

    chore: create dimmable artifact interface

---
+++ src/env/artifact/environment/roomartifact/DimmableArtifact.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2023. Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package artifact.environment.roomartifact;
+
+/**
+ * Interface that models a dimmable artifact, that is an artifacts that offers method to set the intensity of it.
+ */
+public interface DimmableArtifact {
+    /**
+     * Set the intensity of the artifact.
+     * @param intensityPercentage the intensity as a percentage.
+     */
+    void setIntensity(int intensityPercentage);
+}
