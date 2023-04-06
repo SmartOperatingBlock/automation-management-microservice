428b0325e270f0317dfe046ede363741c7146472
Andrea Giulianelli <andrygiulio@gmail.com>
2023-04-06 17:41:29 +0200

    chore: create actuator type concept

---
+++ src/env/entity/actuator/ActuatorType.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2023. Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package entity.actuator;
+
+/**
+ * Enum that describes all the actuators type.
+ */
+public enum ActuatorType {
+    /** Heating. */
+    HEATING,
+    /** Cooling. */
+    COOLING,
+    /** Ventilation. */
+    VENTILATION,
+    /** Ambient light. */
+    AMBIENT_LIGHT,
+    /** Surgical light. */
+    SURGICAL_LIGHT
+}
