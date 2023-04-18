fd890300590fd82d3c3c006985a6d040c224ff30
Andrea Giulianelli <andrygiulio@gmail.com>
2023-04-18 17:51:10 +0200

    build: add toolchain resolver plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,8 +7,9 @@
  */
 
 plugins {
-    id("com.gradle.enterprise") version "3.13"
     id("org.danilopianini.gradle-pre-commit-git-hooks") version "1.1.6"
+    id("com.gradle.enterprise") version "3.13"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.4.0"
 }
 
 rootProject.name = "automation-management-microservice"
