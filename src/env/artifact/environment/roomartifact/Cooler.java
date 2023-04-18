31effc55ffb267141b8c2389f55db3ca18a4457b
Andrea Giulianelli <andrygiulio@gmail.com>
2023-04-18 16:53:03 +0200

    feat: implement cooler actions

--- src/env/artifact/environment/roomartifact/Cooler.java
+++ src/env/artifact/environment/roomartifact/Cooler.java
@@ -10,7 +10,9 @@ package artifact.environment.roomartifact;
 
 import cartago.OPERATION;
 import entity.actuator.ActuatorType;
+import entity.actuator.SwitchableActuator;
 import entity.room.RoomID;
+import infrastructure.wot.WotInvoker;
 
 import java.util.logging.Logger;
 
@@ -19,23 +21,29 @@ import java.util.logging.Logger;
  * to the cooling part.
  */
 public class Cooler extends AbstractActuatorInRoomArtifact implements SwitchableArtifact {
+    private SwitchableActuator actuator;
+    private WotInvoker wotInvoker;
     /**
      * Initialize the cooler artifact.
      * @param roomId the room id where the cooler is placed.
      */
     void init(final String roomId) {
         super.init(ActuatorType.COOLING, new RoomID(roomId));
+        this.actuator = new SwitchableActuator(this.getActuatorID());
+        this.wotInvoker = new WotInvoker();
     }
 
     @Override
     @OPERATION
     public final void turnOn() {
         Logger.getLogger(Cooler.class.toString()).info("[" + this.getRoomId() + "] " + this.getActuatorID().getId() + " ON");
+        this.actuator.turnOn(this.wotInvoker);
     }
 
     @Override
     @OPERATION
     public final void turnOff() {
         Logger.getLogger(Cooler.class.toString()).info("[" + this.getRoomId() + "] " + this.getActuatorID().getId() + " OFF");
+        this.actuator.turnOff(this.wotInvoker);
     }
 }
