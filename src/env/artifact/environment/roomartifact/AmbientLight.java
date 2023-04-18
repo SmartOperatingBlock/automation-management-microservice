fa9c4b401d859af1526f1aab2e0ccda23c555f9c
Andrea Giulianelli <andrygiulio@gmail.com>
2023-04-18 16:54:52 +0200

    feat: implement ambient light actions

--- src/env/artifact/environment/roomartifact/AmbientLight.java
+++ src/env/artifact/environment/roomartifact/AmbientLight.java
@@ -10,7 +10,9 @@ package artifact.environment.roomartifact;
 
 import cartago.OPERATION;
 import entity.actuator.ActuatorType;
+import entity.actuator.DimmableActuator;
 import entity.room.RoomID;
+import infrastructure.wot.WotInvoker;
 
 import java.util.logging.Logger;
 
@@ -19,12 +21,16 @@ import java.util.logging.Logger;
  * Operating Block.
  */
 public class AmbientLight extends AbstractActuatorInRoomArtifact implements DimmableArtifact {
+    private DimmableActuator actuator;
+    private WotInvoker wotInvoker;
     /**
      * Initialize the ambient light artifact.
      * @param roomId the room id where the ambient light is placed.
      */
     void init(final String roomId) {
         super.init(ActuatorType.AMBIENT_LIGHT, new RoomID(roomId));
+        this.actuator = new DimmableActuator(this.getActuatorID());
+        this.wotInvoker = new WotInvoker();
     }
 
     @Override
@@ -32,5 +38,6 @@ public class AmbientLight extends AbstractActuatorInRoomArtifact implements Dimm
     public final void setIntensity(final int luxToSet) {
         Logger.getLogger(AmbientLight.class.toString())
               .info("[" + this.getRoomId().getId() + "] " + this.getActuatorID().getId() + " Set " + luxToSet + " LUX");
+        this.actuator.setIntensity(luxToSet, this.wotInvoker);
     }
 }
