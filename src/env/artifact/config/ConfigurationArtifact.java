be2ed5cc0c80991211975ee46812ea72d0aa8209
Andrea Giulianelli <andrygiulio@gmail.com>
2023-04-19 18:47:10 +0200

    refactor: use file configuration loader

--- src/env/artifact/config/ConfigurationArtifact.java
+++ src/env/artifact/config/ConfigurationArtifact.java
@@ -8,19 +8,11 @@
 
 package artifact.config;
 
+import application.controller.manager.ConfigurationLoader;
 import cartago.Artifact;
 import cartago.OPERATION;
 import application.presenter.configuration.model.Configuration;
-import org.yaml.snakeyaml.DumperOptions;
-import org.yaml.snakeyaml.LoaderOptions;
-import org.yaml.snakeyaml.Yaml;
-import org.yaml.snakeyaml.constructor.Constructor;
-import org.yaml.snakeyaml.representer.Representer;
-
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
+import infrastructure.configuration.FileConfigurationLoader;
 import java.util.Objects;
 
 /**
@@ -40,58 +32,47 @@ public class ConfigurationArtifact extends Artifact {
      */
     @OPERATION
     void load() {
-        final Representer represent = new Representer(new DumperOptions());
-        represent.getPropertyUtils().setSkipMissingProperties(true);
-        final Yaml yaml = new Yaml(new Constructor(Configuration.class, new LoaderOptions()), represent);
-        try (
-                BufferedInputStream configInputStream = new BufferedInputStream(
-                        new FileInputStream(System.getenv(CONFIGURATION_PATH_VARIABLE))
-                )
-        ) {
-            final String fileContent = new String(configInputStream.readAllBytes(), StandardCharsets.UTF_8);
-            final Configuration config = yaml.load(fileContent);
-            // Signal the configuration to the observers
-            // Operating room environment configuration
-            signal("operatingRoom",
-                    config.getOperatingRoom().getTemperature(),
-                    config.getOperatingRoom().getHumidity(),
-                    config.getOperatingRoom().getAmbientIlluminance(),
-                    config.getOperatingRoom().getSurgicalIlluminance());
+        final ConfigurationLoader configurationLoader =
+                new FileConfigurationLoader(System.getenv(CONFIGURATION_PATH_VARIABLE));
+        final Configuration configuration = configurationLoader.loadConfiguration();
+        // Signal the configuration to the observers
+        // Operating room environment configuration
+        signal("operatingRoom",
+                configuration.getOperatingRoom().getTemperature(),
+                configuration.getOperatingRoom().getHumidity(),
+                configuration.getOperatingRoom().getAmbientIlluminance(),
+                configuration.getOperatingRoom().getSurgicalIlluminance());
 
-            // Pre post operating room environment configuration
-            signal("preOperatingRoom",
-                    config.getPrePostOperatingRoom().getTemperature(),
-                    config.getPrePostOperatingRoom().getHumidity(),
-                    config.getPrePostOperatingRoom().getAmbientIlluminance());
+        // Pre post operating room environment configuration
+        signal("preOperatingRoom",
+                configuration.getPrePostOperatingRoom().getTemperature(),
+                configuration.getPrePostOperatingRoom().getHumidity(),
+                configuration.getPrePostOperatingRoom().getAmbientIlluminance());
 
-            // Operating room standby mode
-            if (config.getOperatingRoomStandbyMode().isEnabled()) {
-                signal("operatingRoomStandby",
-                        config.getOperatingRoomStandbyMode().getMinutesToEnable(),
-                        config.getOperatingRoomStandbyMode().getEnvironment().getTemperature(),
-                        config.getOperatingRoomStandbyMode().getEnvironment().getHumidity(),
-                        config.getOperatingRoomStandbyMode().getEnvironment().getAmbientIlluminance(),
-                        config.getOperatingRoomStandbyMode().getEnvironment().getSurgicalIlluminance());
-            }
-
-            // Pre post operating room standby mode
-            if (config.getPrePostOperatingRoomStandbyMode().isEnabled()) {
-                signal("prePostOperatingRoomStandby",
-                        config.getPrePostOperatingRoomStandbyMode().getMinutesToEnable(),
-                        config.getPrePostOperatingRoomStandbyMode().getEnvironment().getTemperature(),
-                        config.getPrePostOperatingRoomStandbyMode().getEnvironment().getHumidity(),
-                        config.getPrePostOperatingRoomStandbyMode().getEnvironment().getAmbientIlluminance());
-            }
-
-            // Medical technologies scenarios
-            config.getMedicalTechnologyScenarios().forEach(scenario -> {
-                signal("medicalTechnologyScenario",
-                        scenario.getMedicalTechnologyType(),
-                        scenario.getAmbientIlluminance(),
-                        scenario.getSurgicalIlluminance());
-            });
-        } catch (IOException e) {
-            throw new IllegalStateException("Impossible to read configuration file", e);
+        // Operating room standby mode
+        if (configuration.getOperatingRoomStandbyMode().isEnabled()) {
+            signal("operatingRoomStandby",
+                    configuration.getOperatingRoomStandbyMode().getMinutesToEnable(),
+                    configuration.getOperatingRoomStandbyMode().getEnvironment().getTemperature(),
+                    configuration.getOperatingRoomStandbyMode().getEnvironment().getHumidity(),
+                    configuration.getOperatingRoomStandbyMode().getEnvironment().getAmbientIlluminance(),
+                    configuration.getOperatingRoomStandbyMode().getEnvironment().getSurgicalIlluminance());
         }
+
+        // Pre post operating room standby mode
+        if (configuration.getPrePostOperatingRoomStandbyMode().isEnabled()) {
+            signal("prePostOperatingRoomStandby",
+                    configuration.getPrePostOperatingRoomStandbyMode().getMinutesToEnable(),
+                    configuration.getPrePostOperatingRoomStandbyMode().getEnvironment().getTemperature(),
+                    configuration.getPrePostOperatingRoomStandbyMode().getEnvironment().getHumidity(),
+                    configuration.getPrePostOperatingRoomStandbyMode().getEnvironment().getAmbientIlluminance());
+        }
+
+        // Medical technologies scenarios
+        configuration.getMedicalTechnologyScenarios().forEach(scenario ->
+                signal("medicalTechnologyScenario",
+                    scenario.getMedicalTechnologyType(),
+                    scenario.getAmbientIlluminance(),
+                    scenario.getSurgicalIlluminance()));
     }
 }
