5cc65c39982b2bee35807ea00a4a0fc8f1a159ac
Andrea Giulianelli <andrygiulio@gmail.com>
2023-04-23 16:36:05 +0200

    chore: create custom light setup event payload interface

---
+++ src/env/application/presenter/event/model/automation/request/customlight/payload/CustomLightSetupPayload.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2023. Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package application.presenter.event.model.automation.request.customlight.payload;
+
+/**
+ * Interface that identify a data payload that is accepted inside a
+ * {@link application.presenter.event.model.automation.request.customlight.CustomLightRequestEvent}.
+ */
+public interface CustomLightSetupPayload { }
